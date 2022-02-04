43e6e14e27d261c5f8adb0bbfed5a7cb7f200ea5
khottinger <khottinger12@comcast.net>
2022-02-04 12:05:54 -0800

    test that will pass

--- SubTest.java
+++ SubTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SubTest {
     @Test
     public void SubtractionTest() {
-        assertEquals(0, Sub.Subtraction(1,4)); //will fail
+        assertEquals(0, Sub.Subtraction(1,1)); //will pass
     }
 }
